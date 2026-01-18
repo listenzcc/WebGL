e0eb7b39d756109f6dc03de2c8426572939dd94d
listenzcc <listenzcc@mail.bnu.edu.cn>
2026-01-18 21:50:28 +0800

    Add Tailwind CSS setup with custom configurations and scripts
    - Created package.json to manage dependencies including Tailwind CSS and http-server.
    - Added serve-forever.ps1 script to run the http-server.
    - Introduced tailwind.input.css for Tailwind CSS configurations and custom styles.
    - Generated tailwind.output.css with compiled styles from the input file.
    - Added tailwind.watch.ps1 script to watch for changes in the CSS and recompile.

---
+++ serve-forever.ps1
@@ -0,0 +1,3 @@
+# file: http-server.ps1
+
+node_modules/.bin/http-server.ps1
---
+++ tailwind.watch.ps1
@@ -0,0 +1,2 @@
+# file: tailwindcss-watch.ps1
+npx @tailwindcss/cli -i ./src/tailwind.input.css -o ./src/tailwind.output.css --watch
